82b46531801cd57dc75c9df73e99b3b4f5b66bb3
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-09-14 18:48:33 +0200

    test all config options

--- cmd/cluster-agent/main.go
+++ cmd/cluster-agent/main.go
@@ -18,7 +18,7 @@ import (
 	_ "expvar"
 
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/cluster"
-	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/network"
+	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/net"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/system"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
